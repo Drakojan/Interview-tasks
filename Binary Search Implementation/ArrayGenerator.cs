7232751c46f7640fda5fa2a4fd982a9f8c9724b6
Nikolay Mitrev <nikolay.mitrev1@gmail.com>
2020-08-25 00:14:09 +0300

    Initial commit

---
+++ Binary Search Implementation/ArrayGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Binary_Search_Implementation
+{
+    public class ArrayGenerator
+    {
+        public ArrayGenerator(int min, int max)
+        {
+            this.Min = min;
+            this.Max = max;
+        }
+
+        private int Min { get; }
+        private int Max { get; }
+
+        public HashSet<int> GenerateArray ()
+        {
+            var random = new Random();
+
+            var collection = new HashSet<int>();
+
+            while (collection.Count<100)
+            {
+                collection.Add(random.Next(Min, Max));
+            }
+
+            return collection;
+        }     
+    }
+}
---
+++ Binary Search Implementation/StartUp.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Binary_Search_Implementation
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the int Array - each int separated by a space or input \"Seed\" for a Random Array of 100 unique numbers");
+
+            var input = Console.ReadLine();
+            var inputList = new List<int>();
+
+            if (input.ToLower() == "seed")
+            {
+                inputList = GenerateArray();
+            }
+
+            else
+            {
+                inputList = input
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .OrderBy(x => x)
+                    .ToList();
+            }
+
+            Console.WriteLine("the sorted array looks like this:");
+            Console.WriteLine(string.Join(" ", inputList));
+
+            Console.WriteLine("Please input the number you are looking for");
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch(inputList.ToArray(), number));
+        }
+
+        private static List<int> GenerateArray()
+        {
+            List<int> inputList;
+
+            var min = 0;
+            var max = 0;
+            bool validInput = true;
+
+            while (validInput)
+            {
+                Console.WriteLine("Please input the min and max values for the numbers in the array each on a new line");
+
+                min = int.Parse(Console.ReadLine());
+                max = int.Parse(Console.ReadLine());
+
+                if (max - min < 100)
+                {
+                    Console.WriteLine("Please provide a valid interval of atleast 100 numbers");
+                    continue;
+                }
+                validInput = false;
+            }
+
+            var generator = new ArrayGenerator(min, max);
+            inputList = generator.GenerateArray()
+                .OrderBy(x => x)
+                .ToList();
+            return inputList;
+        }
+
+        public static string BinarySearch(int[] inputArray, int number)
+        {
+            int lowBoundryIndex = 0;
+            int highBoundryIndex = inputArray.Length - 1;
+            int middleIndex = 0;
+
+            int counter = 0;
+
+            while (lowBoundryIndex<=highBoundryIndex)
+            {
+                middleIndex = (lowBoundryIndex + highBoundryIndex) / 2;
+
+                if (inputArray[middleIndex] == number)
+                {
+                    return $"The number you are looking for is at index {middleIndex}. It was found in {counter} iterations";
+                    //note that the search returns the first encounter if there are duplicates of the number we are looking for
+                }
+
+                if (inputArray[middleIndex] > number)
+                {
+                    highBoundryIndex = middleIndex - 1;
+                }
+
+                if (inputArray[middleIndex] < number)
+                {
+                    lowBoundryIndex = middleIndex + 1;
+                }
+
+                counter++;
+            }
+            return $"The number you are looking for does not exist in the given array. This was established in {counter} operations";
+        }
+    }
+}
